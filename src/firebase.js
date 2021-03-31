f8e6607da46e6220997a0cae0d2e6e55ed3017bf
Dez <dez@dezs-mbp.attlocal.net>
2021-03-31 13:30:43 -0400

    firebase config add

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAa-TG--v5eT-60_6e9MO-78BhelWplRiA",
+  authDomain: "dezflix-clone.firebaseapp.com",
+  projectId: "dezflix-clone",
+  storageBucket: "dezflix-clone.appspot.com",
+  messagingSenderId: "720110595046",
+  appId: "1:720110595046:web:0511b4bc11d3afd2358427",
+};
