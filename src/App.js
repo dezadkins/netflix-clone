ab117eb82d9556edfde78575653850e940f52207
Dez <desmondadkins@gmail.com>
2021-04-01 13:13:00 -0400

    homescreen in

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
+
 import "./App.css";
+import HomeScreen from "./HomeScreen/HomeScreen";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <HomeScreen />
+    </div>
+  );
 }
 
 export default App;
