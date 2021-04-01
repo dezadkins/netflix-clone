d3f3ee6dbcb980d36859de24328cd590fd25d406
Dez <desmondadkins@gmail.com>
2021-04-01 13:57:25 -0400

    nav bar render and unrender

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Nav.css";
 
 function Nav() {
+  const [show, handleShow] = useState(false);
+
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    // Line below is clean up
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
+
   return (
-    <div className="nav nav__black">
+    //   only add nav__black class if (render) nav if show variable true
+    <div className={`nav ${show && "nav__black"}`}>
       <div className="nav__contents">
         <img
           className="nav__logo"
