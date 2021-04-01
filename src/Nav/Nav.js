0269519a2ce38661f387f32a1bb24481df1321c0
Dez <desmondadkins@gmail.com>
2021-04-01 13:34:38 -0400

    avatar and logo add

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -4,10 +4,19 @@ import "./Nav.css";
 function Nav() {
   return (
     <div className="nav">
-      <img
-        src="https://variety.com/wp-content/uploads/2020/05/netflix-logo.png?w=1024"
-        alt=""
-      />
+      <div className="nav__contents">
+        <img
+          className="nav__logo"
+          src="https://variety.com/wp-content/uploads/2020/05/netflix-logo.png?w=1024"
+          alt=""
+        />
+
+        <img
+          className="nav__avatar"
+          src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+          alt=""
+        />
+      </div>
     </div>
   );
 }
