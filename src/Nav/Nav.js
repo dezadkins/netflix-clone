c2960ae35d0d687c6b6e90e47ef0330e7ead6b4c
Dez <desmondadkins@gmail.com>
2021-04-01 13:44:22 -0400

    header nav going

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -3,11 +3,11 @@ import "./Nav.css";
 
 function Nav() {
   return (
-    <div className="nav">
+    <div className="nav nav__black">
       <div className="nav__contents">
         <img
           className="nav__logo"
-          src="https://variety.com/wp-content/uploads/2020/05/netflix-logo.png?w=1024"
+          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
           alt=""
         />
 
