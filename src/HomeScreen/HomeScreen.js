a6bb2f12e8d15020b5b3642716a900a8d703ef26
Dez <desmondadkins@gmail.com>
2021-04-07 16:35:05 -0400

    homescreen fetch rows done

--- src/HomeScreen/HomeScreen.js
+++ src/HomeScreen/HomeScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import Banner from "../Banner/Banner";
 import Nav from "../Nav/Nav";
 import requests from "../Requests";
+import Row from "../Row/Row";
 
 import "./HomeScreen.css";
 
@@ -20,10 +21,13 @@ function HomeScreen() {
         fetchUrl={requests.fetchNetflixOriginals}
         isLargeRow
       />
-      <Row />
-      <Row />
-      <Row />
-      <Row />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
