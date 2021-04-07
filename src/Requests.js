8d98edd6b759c85e595bf51bec498af1ddaa6b86
Dez <desmondadkins@gmail.com>
2021-04-07 15:33:53 -0400

    api key and requests setup

---
+++ src/Requests.js
@@ -0,0 +1,14 @@
+// Usually would store in {process.env.API_KEY}
+
+const API_KEY = "36af47f98a672224db397aabd164ff4c";
+
+const requests = {
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}$language=en-US`,
+};
--- src/axios.js
+++ src/axios.js
@@ -1,4 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
+const instance = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+});
 
-36af47f98a672224db397aabd164ff4c
+export default instance;
