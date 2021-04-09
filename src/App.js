6092675defa853d6cbc9e5d07ad169a520ad65c5
Dez <desmondadkins@gmail.com>
2021-04-09 13:02:59 -0400

    login screen setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomeScreen from "./HomeScreen/HomeScreen";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./Login/Login";
 
 function App() {
+  const user = null;
   return (
     <div className="app">
-      <HomeScreen />
       <Router>
-        <Switch>
-          <Route path="/">
-            <HomeScreen />
-          </Route>
-        </Switch>
+        {!user ? (
+          <Login />
+        ) : (
+          <Switch>
+            <Route exact path="/">
+              <HomeScreen />
+            </Route>
+          </Switch>
+        )}
       </Router>
     </div>
   );
