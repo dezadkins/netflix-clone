eb5895c057d455135157442fc9df565e74273ecf
Dez <desmondadkins@gmail.com>
2021-04-14 10:48:32 -0400

    email and password ref setup

--- src/SignIn/SignIn.js
+++ src/SignIn/SignIn.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useRef } from "react";
+import { auth } from "../firebase";
 import "./SignIn.css";
 function SignIn() {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const register = (e) => {
+    e.preventDefault();
+    auth.createUserWithEmailAndPassword(
+      emailRef.current.value,
+      passwordRef.current.value
+    );
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="signIn">
       <form action="">
         <h1>Sign In</h1>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Sign In</button>
+        <input type="email" placeholder="Email" ref={emailRef} />
+        <input type="password" placeholder="Password" ref={passwordRef} />
+        <button type="submit" onClick={signIn}>
+          Sign In
+        </button>
         <h4>
           <span className="signIn__gray">New to Netflix? </span>
 
-          <span className="signIn__link">
+          <span className="signIn__link" onClick={register}>
             <a href="">Sign Up now.</a>
           </span>
         </h4>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAa-TG--v5eT-60_6e9MO-78BhelWplRiA",
   authDomain: "dezflix-clone.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
   messagingSenderId: "720110595046",
   appId: "1:720110595046:web:0511b4bc11d3afd2358427",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
