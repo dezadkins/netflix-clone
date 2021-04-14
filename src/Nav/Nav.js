bdd00052f5d5d5b2253ebf85cb2ace2fc88f9ace
Dez <desmondadkins@gmail.com>
2021-04-14 12:05:15 -0400

    logo fix

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -24,7 +24,7 @@ function Nav() {
       <div className="nav__contents">
         <img
           className="nav__logo"
-          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          src="https://i.imgur.com/EdyiN6K.png"
           alt=""
         />
 
