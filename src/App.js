72b6fff6c5a81ce44f78d01358997c57757930b4
Dez <desmondadkins@gmail.com>
2021-04-14 11:25:22 -0400

    login useeffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import HomeScreen from "./HomeScreen/HomeScreen";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./Login/Login";
+import { auth } from "./firebase";
+
+useEffect(() => {
+  const getOut = auth.onAuthStateChanged((userAuth) => {
+    if (userAuth) {
+      //Logged In
+    } else {
+      //Loggged Out
+    }
+  });
+
+  return getOut; //performance not effected with this.
+}, []);
 
 function App() {
   const user = null;
--- src/SignIn/SignIn.js
+++ src/SignIn/SignIn.js
@@ -7,14 +7,30 @@ function SignIn() {
 
   const register = (e) => {
     e.preventDefault();
-    auth.createUserWithEmailAndPassword(
-      emailRef.current.value,
-      passwordRef.current.value
-    );
+    auth
+      .createUserWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   };
 
   const signIn = (e) => {
     e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => alert(error.message));
   };
 
   return (
