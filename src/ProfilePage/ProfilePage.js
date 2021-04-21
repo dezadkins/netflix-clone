29ed90940cb83a2fb3c03f517f11fcc4285b1444
Dez <desmondadkins@gmail.com>
2021-04-21 14:20:53 -0400

    profile page signout functional with min data

--- src/ProfilePage/ProfilePage.js
+++ src/ProfilePage/ProfilePage.js
@@ -20,6 +20,8 @@ function ProfilePage() {
           <div className="profile__details">
             <h2>{user.email}</h2>
             <div className="profile__plans">
+              <h3>Plans</h3>
+              <p>Renewal Date: </p>
               <button
                 onClick={() => auth.signOut()}
                 className="profile__signOut"
